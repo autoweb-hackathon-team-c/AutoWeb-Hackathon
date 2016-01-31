0cc761e223c4510c74baa4cd26965994aeb087e3
guosheng-lin <guosheng1977@gmail.com>
2016-01-31 14:22:05 +0900

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,21 @@
 ion.sound({
   sounds: [{
-    name: "nyanpass",
+    name: "sheetbelt",
     volume: 0.2
   }, {
     name: "headlight",
+    volume: 0.2
+  },{
+    name: "anzen",
+    volume: 0.2
+  },{
+    name: "chikasugi",
+    volume: 0.2
+  },{
+    name: "furatuki",
+    volume: 0.2
+  },{
+    name: "otukare",
     volume: 0.2
   }],
   volume: 0.5,
